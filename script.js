f47b8cb9dbb00854df9d12286617c57513bd2a67
Antara Paul <hiiamantara04@gmail.com>
2024-03-31 23:30:49 +0530

    About page + scroll effect

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
