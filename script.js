70eeaf378b954542dc99c36a21ab2d3dd627557b
Antara Paul <hiiamantara04@gmail.com>
2024-05-05 15:48:52 +0530

    functional project section

--- script.js
+++ script.js
@@ -12,48 +12,73 @@ document.querySelectorAll('.nav-link').forEach(anchor => {
     });
 });
 
-    // Get references to the arrow buttons, textbox head, textbox paragraph, and exp image
-    const arrowLeft = document.querySelector('.arrow-l');
-    const arrowRight = document.querySelector('.arrow-r');
-    const textBoxHead = document.querySelector('.textbox-head');
-    const textBoxP = document.querySelector('.textbox-p');
-    const expImage = document.querySelector('.exp img');
+const arrowLeft = document.querySelector('.arrow-l');
+const arrowRight = document.querySelector('.arrow-r');
+const projectImagesContainer1 = document.querySelectorAll(".project-img1");
+const projectImagesContainer2 = document.querySelectorAll(".project-img2");
+const projectImagesContainer3 = document.querySelectorAll(".project-img3");
+const textBoxHead = document.querySelector('.textbox-head');
+const textBoxP = document.querySelector('.textbox-p');
+const expImage = document.querySelector('.exp img');
 
-    // Define the content arrays for the textbox head, textbox paragraph, and exp image
-    const textBoxHeadContent = ['DEVFEST KOLKATA 2023', 'REBASE', ' '];
-    const textBoxPContent = [
-        'I designed this website as a part of the organizing team of Devfest Kolkata 2023.',
-        'I designed this website as a part of the organizing team of Rebase. Took me a long long time T-T',
-        'You can find all my other design projects on my Dribble and Behance <3 All my coding projects are available on my Github, including the repo for this website ;)'
-    ];
-    const expImageSrc = [
-        'assets/images/exp1.png',
-        'assets/images/exp2.png',
-        'assets/images/exp3.png'
-    ];
+const projectSets = [
+    [
+        "assets/images/Landing Page.png",
+        "assets/images/About Page.png",
+        "assets/images/faq.png"
+    ],
+    [
+        "assets/images/Devfest Image 1.png",
+        "assets/images/Devfest Image 2.png",
+        "assets/images/Devfest Image 3.png"
+    ],
+    ["","",""]
+];
 
-    // Initialize the index to track the current content
-    let currentIndex = 0;
+const textBoxHeadContent = ['REBASE','DEVFEST KOLKATA 2023', ' '];
+const textBoxPContent = [
+    'I designed this website as well as the branding for the hackathon as a part of the organizing team of Rebase. Took me a long long time T-T',
+    'I designed this website as a part of the organizing team of Devfest Kolkata 2023.',
+    'You can find all my other design projects on my Dribble and Behance <3 All my coding projects are available on my Github, including the repo for this website ;)'
+];
 
-    // Function to update the content based on the current index
-    function updateContent() {
-        textBoxHead.textContent = textBoxHeadContent[currentIndex];
-        textBoxP.textContent = textBoxPContent[currentIndex];
-        expImage.src = expImageSrc[currentIndex];
-    }
+const expImageSrc = [
+    'assets/images/exp2.png',
+    'assets/images/exp1.png',
+    'assets/images/exp3.png'
+];
 
-    // Event listener for the left arrow button
-    arrowLeft.addEventListener('click', () => {
-        currentIndex = (currentIndex - 1 + textBoxHeadContent.length) % textBoxHeadContent.length;
-        updateContent();
+let currentIndex = 0;
+
+function updateContent() {
+    textBoxHead.textContent = textBoxHeadContent[currentIndex];
+    textBoxP.textContent = textBoxPContent[currentIndex];
+    expImage.src = expImageSrc[currentIndex];
+
+    const currentProjectSet = projectSets[currentIndex];
+
+    projectImagesContainer1.forEach((container, index) => {
+        container.querySelector('img').src = currentProjectSet[index];
     });
 
-    // Event listener for the right arrow button
-    arrowRight.addEventListener('click', () => {
-        currentIndex = (currentIndex + 1) % textBoxHeadContent.length;
-        updateContent();
+    projectImagesContainer2.forEach((container, index) => {
+        container.querySelector('img').src = currentProjectSet[index+1];
     });
 
-    // Initial content update
+    projectImagesContainer3.forEach((container, index) => {
+        container.querySelector('img').src = currentProjectSet[index+2];
+    });
+}
+
+arrowLeft.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + textBoxHeadContent.length) % textBoxHeadContent.length;
     updateContent();
+});
 
+arrowRight.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % textBoxHeadContent.length;
+    updateContent();
+});
+
+// Initial content update
+updateContent();
